49e02cfa2e918a6086db3cf73c82c202281991e1
Strangers123 <91206077+Tsundere123@users.noreply.github.com>
2023-11-23 12:35:48 +0100

    No

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 //        String[] listOfTopics = new String[];
-
+        System.out.println("Yes");
+        System.out.println("no...");
     }
 
 
